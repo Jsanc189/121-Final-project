2142aa4a0728cbd77519c56709b165055659187c
egitelma <egitelma@ucsc.edu>
2024-11-22 13:49:28 -0800

    Player can now click on cells, and trigger an event when clicking on an adjacent one

--- src/Scripts/Grid.js
+++ src/Scripts/Grid.js
@@ -17,10 +17,14 @@ export class Grid {
     for(let x = 0; x < height; x++){
       this.tiles[x] = [];
       for(let y = 0; y < width; y++){
-        let tileWeather = new TileWeather(x, y, this.weather).generate();
-        this.tiles[x].push(new Cell(
-          x, y, tileWeather.sun, tileWeather.rain
-        ));
+        const tileWeather = new TileWeather(x, y, this.weather).generate();
+        const newTile = new Cell(x, y, tileWeather.sun, tileWeather.rain);
+        this.tiles[x].push(newTile);
+
+        // scene.graphics.setInteractive({ useHandCursor: true, 
+        //     hitArea: newTile.rect,
+        //     hitAreaCallback: Phaser.Geom.Rectangle.Contains,
+        // })
       }
     }
     console.log(this.tiles)
@@ -63,6 +67,15 @@ export class Grid {
   getCellAt(x, y, tile_size){
     return this.tiles[Math.floor(y / tile_size)][Math.floor(x / tile_size)];
   }
+
+  isAdjacentCell(cell1, cell2){
+    console.log(cell1.x - cell2.x);
+    console.log(cell1.y - cell2.y)
+    if(Math.abs(cell1.x - cell2.x) == 1 || Math.abs(cell1.y - cell2.y) == 1){
+      return true;
+    }
+    return false;
+  }
 }
 
 class Cell {
@@ -72,6 +85,7 @@ class Cell {
     this.sun_lvl = sun_lvl;
     this.rain_lvl = rain_lvl;
     this.plant = plant;
+    // this.rect = new Phaser.Geom.Rectangle(x * 40, y * 40, 40, 40);
   }
 
   updateWeatherAtCell(sun_lvl, rain_lvl){
