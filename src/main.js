674673c8ad474d5c25280589a3939053467c1c56
violetferret <kahlstro@ucsc.edu>
2024-11-27 10:43:32 -0800

    merging with jackie's code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Game, } from "phaser";
+import { Game } from "phaser";
 import { Load } from "./Scenes/Load.js";
 import { blankScene } from "./Scenes/Scene.js";
 import { PlayScene } from "./Scenes/Play.js";
@@ -6,15 +6,15 @@ import { MenuScene } from "./Scenes/Menu.js";
 
 // game config
 let config = {
-    parent: 'phaser-game',
-    type: Phaser.AUTO,
-    render: {
-        pixelArt: true  // prevent pixel art from getting blurred when scaled
-    },
-    width: 800,
-    height: 900,
-    scene: [Load, MenuScene, PlayScene]
-}
+  parent: "phaser-game",
+  type: Phaser.AUTO,
+  render: {
+    pixelArt: true, // prevent pixel art from getting blurred when scaled
+  },
+  width: 800,
+  height: 900,
+  scene: [Load, MenuScene, PlayScene],
+};
 
 const game = new Game(config);
 
