9512f64c7f469c804acb88a339bdd8bda2869f7e
violetferret <kahlstro@ucsc.edu>
2024-12-04 15:25:27 -0800

    adding langauges scene

---
+++ src/Scenes/Languages.ts
@@ -0,0 +1,19 @@
+export class LanguagesScene extends Phaser.Scene {
+    constructor() {
+      super("languagesScene");
+    }
+  
+    preload() {
+    }
+  
+    init() {
+    }
+  
+    create() {
+      console.log("Scene loaded");
+    }
+  
+    update() {
+    }
+  }
+  
