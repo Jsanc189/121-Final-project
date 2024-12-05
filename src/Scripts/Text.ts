5235a0eacc0ee391aed7f6b1a5cb47840d9a22ab
violetferret <kahlstro@ucsc.edu>
2024-12-04 20:05:39 -0800

    started on translations, framework to be refined

---
+++ src/Scripts/Text.ts
@@ -0,0 +1,75 @@
+const eng_strings: String[] = [
+    "Plant Harvest",
+    "New Game",
+    "Load Game",
+    "Save",
+    "Back",
+    "Delete saves",
+    "empty slot",
+    "Undo",
+    "Redo",
+    "End Day",
+    "Save",
+    "Quit",
+    "Autosave",
+    "Weather layer",
+    "sun",
+    "rain"
+]
+
+const swe_strings: String[] = [
+    "Växtskörd",
+    "Nytt Spel",
+    "Ladda Spel",
+    "Spara",
+    "Tillbaka",
+    "Radera saves",
+    "tom save",
+    "Ångra",
+    "Gör om",
+    "Avsluta dagen",
+    "Spara",
+    "Avsluta spel",
+    "Autospara",
+    "Visa väder",
+    "sol",
+    "regn"
+];
+
+const hebr_strings: String[] = [
+    "קציר צמחים",
+    "משחק חדש",
+    "לטעון משחק",
+    "שמירה",
+    "הקודם",
+    "למחוק את השמירות",
+    "שמירה ריקה",
+    "ביטול פעולה",
+    "עושת שוב",
+    "מסיים את היום",
+    "שומר את המשחק",
+    "עזוב את המשחק",
+    "שמירה אוטומטית",
+    "מראה את מזג האוויר",
+    "שמש",
+    "גשם"
+]
+
+const strings: String[] = [
+    "Plant Harvest", // name of the game
+    "New Game", // to start a new game
+    "Load Game", // to load a previous save
+    "Save", // indicating a save slot (eg. save 1)
+    "Back", // indicating a "go back" button
+    "Delete saves", // button that deletes all saves
+    "empty slot", // indicates empty save slot
+    "Undo", // undo last game action
+    "Redo", // redo last undid game action
+    "End Day", // end the day, in-game
+    "Save", // save the game, in-game
+    "Quit", // quit the game, in-game
+    "Autosave", // toggle autosave functions
+    "Weather layer", // show the weather layer overlay 
+    "sun", // just sun
+    "rain" // just rain 
+]
